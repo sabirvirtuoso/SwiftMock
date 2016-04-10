27299b11a47d87dd7b1471508000efc33cb92582
Syed Sabir Salman-Al-Musawi <sabirvirtuoso@gmail.com>
2016-04-10 15:32:13 +0600

    Implement optional argument matching

--- Pod/Classes/MockExpectation.swift
+++ Pod/Classes/MockExpectation.swift
@@ -12,6 +12,7 @@ public class MockExpectation {
     /// calls will be matched against this functionName and arguments array
     public var functionName: String?
     var args = [Any?]()
+    var argumentMatcherRequirement =  [Bool]()
 
     /// the actionable object holds actions for this expectation, and can perform them
     var actionPerformer: MockActionPerformer!
@@ -20,7 +21,11 @@ public class MockExpectation {
     }
     
     /// this call makes a MockActionable<T>, where T is the return value of the funciton being mocked. The returned object is resonsible for making and performing Actions when the expectation is satisfied. Every subsequent call to "andDo", "andReturn", etc is a separate Action.
-    public func call<T: Any>(value: T) -> MockActionable<T> {
+    public func call<T: Any>(value: T, withArgumentMatcherRequirement: [Bool]? = nil) -> MockActionable<T> {
+        if let _ = withArgumentMatcherRequirement {
+          argumentMatcherRequirement = withArgumentMatcherRequirement!
+        }
+
         let theActionable = MockActionable(value)
         actionPerformer = theActionable
         return theActionable
@@ -33,6 +38,10 @@ public class MockExpectation {
         if result {
             functionName = theFunctionName
             args = theArgs
+
+            for index in (0..<theArgs.count) {
+              argumentMatcherRequirement[index] = true
+            }
         }
         return result
     }
@@ -44,7 +53,17 @@ public class MockExpectation {
     /// offer this function, and its arguments, to the expectation to see if it matches
     public func satisfy(functionName theFunctionName: String, args theArgs: Any?...) -> Bool {
         let matcher = MockMatcher()
-        return functionName == theFunctionName && matcher.match(args, theArgs)
+        var expectedArgs = [Any?]()
+        var actualArgs = [Any?]()
+
+        for (index, matchArg) in argumentMatcherRequirement.enumerate() {
+          if matchArg {
+            expectedArgs.append(args[index])
+            actualArgs.append(theArgs[index])
+          }
+        }
+
+        return functionName == theFunctionName && matcher.match(expectedArgs, actualArgs)
     }
     
     /// perform actions, and return a value from the mock
